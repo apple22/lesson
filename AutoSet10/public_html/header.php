a0880e8149144b600ec22fbbeffadecb9772a012
hongseongguk <join27833@gmail.com>
2020-06-12 08:26:48 +0900

    test

---
+++ AutoSet10/public_html/header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="/css/index.css">
+    <link rel="stylesheet" type="text/css" href="/css/reset.css">
+    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <script src="/js/common.js"></script>
+    <title>Main_warning_multiple_status</title>
+</head>
+
+<body>
+    <div class="main_header">
+        <div class="main_header_logo">
+            <p class="logo">AVISS</p>
+            <p class="admin">admin</p>
+        </div>
+        <!--main_header_logo-->
+        <div class="main_header_time">
+            <div class="time">2020/07/16</div>
+            <!--time-->
+            <div class="location">
+                <ul>
+                    <li><img src="/img/location.png">울산</li>
+                </ul>
+                <ul>
+                    <li><img src="/img/direction.png">북북동</li>
+                </ul>
+                <ul>
+                    <li><img src="/img/speed.png">3.0</li>
+                </ul>
+            </div>
+            <!--location-->
+        </div>
+        <!--main_header_time-->
+
+        <div class="main_header_status">
+            <ul>
+                <li class="history">History</li>
+                <li class="schedule">도선스케줄확인</li>
+                <li class="login"><img src="./img/normal.png"></li>
+            </ul>
+
+        </div>
+        <!--main_header_status-->
+
+
+    </div>
+    <!--main_header-->
+</body>
+
+</html>
